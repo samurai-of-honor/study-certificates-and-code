67e137d0da4b661885714bf974a9c2cd9fb45bb9
samurai-of-honor <pashabaran1207@gmail.com>
2022-01-27 21:03:48 +0200

    add functions solutions

---
+++ Go/Stepik/6-functions.go
@@ -0,0 +1,59 @@
+package main
+import . "fmt" // . - импортируем все функции пакета
+
+func main() {
+  var some_text string
+  Println("Enter string: ")
+  Scan(&some_text)
+  fn(some_text)
+
+  min := minimumFromFour() // присвоение возвращаемого результата в переменную
+  Println("Minimum number:", min)
+
+  res := vote(1, 0, 0)
+  Println("More votes:", res)
+
+  var n int
+  Println("Enter Fibonacci element index:")
+  Scan(&n)
+  res = fib(n)
+  Println(n, "element Fibonacci:", res)
+}
+
+// Написать функцию, которая будет принимать строку и выводить её
+func fn(text string){
+  Println("Your string:", text)
+}
+
+// Написать функцию, находящую наименьшее из четырех введённых чисел
+func minimumFromFour() int { // тип возвращаемого значения(может быть несколько)
+  var a, min int
+  Println("Enter 4 numbers:")
+  Scan(&min)
+  for i := 1; i < 4; i++ {
+      Scan(&a)
+      if a < min {
+          min = a
+      }
+  }
+  return min
+}
+
+// Написать "функцию голосования", возвращающую 0 или 1,
+// в зависимости от количества аргументов которые встречается чаще
+func vote(x int, y int, z int) ( int ) {
+  if x == y {
+      return x
+  }
+  return z
+}
+
+// Вводится n. Необходимо вывести значение
+// n-ого елемента последовательности Фибоначчи
+func fib(n int) int {
+  var a, b int = 1, 1
+  for i := 1; i < n; i++ {
+    a, b = b, a + b
+  }
+  return a
+}
